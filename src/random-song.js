03525ee495ad57d4458f749037ac4b177ceff99c
Michael Chadwick <michael.chadwick@gmail.com>
2026-07-31 16:34:08 -0700

    added some error checking to api endpoint retrieval

--- src/random-song.js
+++ src/random-song.js
@@ -1,8 +1,13 @@
 export default async function randomSong() {
-  const response = await fetch(
-    "https://music.nebyoolae.com/jsonapi/views/random_song/guess_neb",
-  );
-  const song = await response.json();
+  const API_URL =
+    "https://music.nebyoolae.com/jsonapi/views/random_song/guess_neb";
 
-  return song.data[0].title;
+  try {
+    const response = await fetch(API_URL);
+    const song = await response.json();
+
+    return song.data[0].title;
+  } catch {
+    return null;
+  }
 }
